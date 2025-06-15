ba8c475ab24cd980114657eb2398f8a5826dd641
shariyerShazan <shariyershazan1@gmail.com>
2025-06-15 16:33:10 +0600

    user controller

---
+++ controllers/user.controller.js
@@ -0,0 +1,156 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const register = async (req, res) => {
+  try {
+    const { fullName, email, photoURL, password , role} = req.body;
+    if (!fullName || !email || !photoURL || !password || !role) {
+      return res.status(400).json({
+        message: "Something is missing",
+        success: false,
+      });
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        message: "User already exist with this email",
+        success: false,
+      });
+    }
+
+
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      return res.status(400).json({
+        message: "Please enter a valid email address",
+        success: false,
+      });
+    }
+
+    //  Password validation
+    if (password.length < 6) {
+      return res.status(400).json({
+        message: "Password must be at least 6 characters long",
+        success: false,
+      });
+    }
+
+    if (!/[A-Z]/.test(password)) {
+      return res.status(400).json({
+        message: "Password must contain at least one uppercase letter",
+        success: false,
+      });
+    }
+
+    if (!/[a-z]/.test(password)) {
+      return res.status(400).json({
+        message: "Password must contain at least one lowercase letter",
+        success: false,
+      });
+    }
+
+    const hashedPass = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      fullName,
+      email,
+      photoURL,
+      password: hashedPass,
+      role
+    });
+    await newUser.save();
+
+    return res.status(200).json({
+      message: "User registered successfully",
+      success: true,
+      user: newUser,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password , role} = req.body;
+
+    if (!email || !password || !role) {
+      return res.status(400).json({
+        message: "Something is missing",
+        success: false,
+      });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message: "incorrect email or password",
+        success: false,
+      });
+    }
+    const comparePass = await bcrypt.compare(password, user.password);
+    if (!comparePass) {
+      return res.status(200).json({
+        message: "incorrect email or password",
+        success: false,
+      });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY, {
+      expiresIn: "7d",
+    });
+
+    if(user?.role !== role){
+      return res.status(400).json({
+        message:  `User does not have the required role: ${role}` ,
+        success: false
+      })
+    }
+
+    return res
+      .status(200)
+      .cookie("token", token, {
+        httpOnly: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      })
+      .json({
+        message: `Welcome ${user.fullName}`,
+        success: true,
+        user,
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+
+export const logout = async (req, res) => {
+  try {
+    return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+      message: "User logedout successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+
+
+export const findUserByEmail = async (req, res) => {
+  const { email } = req.params;
+
+  if (!email) return res.status(400).json({ message: "Email is required" });
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
+ 
